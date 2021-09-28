ce57ee5de936b2b344b9a8e35ebc8f06565f3780
Emran <63017060+Emrandotcom@users.noreply.github.com>
2021-09-28 19:17:47 -0400

    Uploaded java files

---
+++ Java Programming Masterclass/Decimal Comparator/DecimalComparator.java
@@ -0,0 +1,12 @@
+public class DecimalComparator {
+    public static boolean areEqualByThreeDecimalPlaces(double num1, double num2){
+
+        int firstNum = (int)(num1 * 1000);
+        int secondNum = (int)(num2 * 1000);
+
+        if (firstNum - secondNum == 0){
+            return true;
+        } else
+            return false;
+    }
+}
---
+++ Java Programming Masterclass/Decimal Comparator/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean result = DecimalComparator.areEqualByThreeDecimalPlaces(3.149,3.1491);
+        System.out.println(result);
+    }
+}
